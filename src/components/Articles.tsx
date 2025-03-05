2bac413564879055ae5e8c042a17aeec99577397
Daniel Gerardo Rondón García <daniel5232010@gmail.com>
2025-03-05 14:59:49 -0500

    fix: se ajusta color en modo claro de card de proectos

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -31,8 +31,8 @@ const Articles: React.FC = () => {
             key={filter}
             onClick={() => setActiveFilter(filter)}
             className={`px-4 py-2 rounded-full text-sm font-semibold transition-colors duration-300 ${activeFilter === filter
-                ? 'bg-[var(--primary-color-500)] text-black'
-                : 'bg-gray-800 text-white hover:bg-gray-700'
+              ? 'bg-[var(--primary-color-500)] text-white dark:text-black'
+              : 'bg-gray-800 text-white hover:bg-gray-700'
               }`}
           >
             {filter}
@@ -42,15 +42,13 @@ const Articles: React.FC = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {filteredArticles.map((article, index) => (
-          <div key={index} className="bg-gray-900 overflow-hidden group transition-all duration-300 relative rounded-lg">
+          <div key={index} className="bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md transition-all duration-300 hover:text-[var(--primary-color-500)] group relative">
             <div className="relative h-48">
-              <img src={article.image} alt={article.title} className="w-full h-full object-cover" />
+              <img src={article.image} alt={article.title} className="w-full h-full object-cover rounded-lg" />
               <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 <a
                   href={article.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[var(--primary-color-500)] text-white px-4 py-2 rounded-full font-semibold hover:text-[var(--primary-color-600)] transition-colors duration-300"
+                  className="text-white dark:text-gray-900 px-4 py-2 rounded-full font-semibold bg-[var(--primary-color-500)] hover:bg-[var(--primary-color-600)] transition-colors duration-300"
                 >
                   Read More
                 </a>
@@ -58,14 +56,15 @@ const Articles: React.FC = () => {
             </div>
             <div className="p-4">
               <span className="text-[var(--primary-color-500)] text-sm font-semibold">{article.category}</span>
-              <h3 className="text-xl font-bold mt-2">{article.title}</h3>
+              <h3 className="text-xl font-bold mt-2 text-gray-900 dark:text-white">{article.title}</h3>
             </div>
+
             {/* Custom shape with two square corners and two rounded corners */}
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
-              <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-green-500"></div>
-              <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-green-500 rounded-tr-lg"></div>
-              <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-green-500 rounded-bl-lg"></div>
-              <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-green-500"></div>
+              <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-[var(--primary-color-500)]"></div>
+              <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-[var(--primary-color-500)] rounded-tr-lg"></div>
+              <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-[var(--primary-color-500)] rounded-bl-lg"></div>
+              <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-[var(--primary-color-500)]"></div>
             </div>
           </div>
         ))}
